9d5ebda9cc073f92ad54264f8ac74aeddcc2f888
butvan <1973578950@qq.com>
2026-04-14 20:20:19 +0800

    Initial Commit

---
+++ sshutil/sshutilApp.swift
@@ -0,0 +1,17 @@
+//
+//  sshutilApp.swift
+//  sshutil
+//
+//  Created by 可梵 on 2026/4/14.
+//
+
+import SwiftUI
+
+@main
+struct sshutilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
